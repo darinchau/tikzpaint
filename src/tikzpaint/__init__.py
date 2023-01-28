dd035b3d1efa178dc31c03f454b670639f1d6a26
Darin Chau <darinchauyf@gmail.com>
2023-01-28 14:39:36 -0800

    Refactored imports

--- src/tikzpaint/__init__.py
+++ src/tikzpaint/__init__.py
@@ -1,1 +1,10 @@
 from tikzpaint.util import Coordinates
+from tikzpaint.figures.drawable import Drawable
+from tikzpaint.figures.displayable import Displayable
+from tikzpaint.figures.figure import Figure
+from tikzpaint.figures.projection import Projection, LinearProjection, StereographicProjection
+from tikzpaint.figures.options import PlotOptions
+from tikzpaint.shapes.arrow import Arrow
+from tikzpaint.shapes.line import Line
+from tikzpaint.shapes.vector import Vector
+from tikzpaint.shapes.point import Point
--- src/tikzpaint/shapes/base/arrow.py
+++ src/tikzpaint/shapes/base/arrow.py
@@ -5,7 +5,7 @@ from matplotlib.axes import Axes
 
 class L0Arrow(Displayable):
     """Implementation of an arrow that could be drawn on a figure. Note: arrows must be straight and they are not
-    reall compatible with stereographic projections
+    really compatible with stereographic projections
     
     start: coordinates of the start of an arrow
     end: coordinates of the end of an arrow"""
